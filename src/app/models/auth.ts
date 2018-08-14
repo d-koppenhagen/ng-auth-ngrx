df85db202c08068539d0b57544cb2c4e19ca028e
Danny Koppenhagen <mail@d-koppenhagen.de>
2018-08-14 16:58:24 +0200

    using async pipe

--- src/app/models/auth.ts
+++ src/app/models/auth.ts
@@ -1,5 +1,16 @@
+import { User } from './user';
+
 export class AuthData {
   token?: string;
   tokenExpires?: number;
   user?: string;
+}
+
+export interface AuthState {
+  // is a user authenticated?
+  auth: AuthData;
+  // if authenticated, there should be a user object
+  user: User | null;
+  // error message
+  errorMessage: string | null;
 }
