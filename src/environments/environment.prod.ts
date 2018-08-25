6be6999c16001f5c159401b1f9ed7e973d122d92
Danny Koppenhagen <mail@d-koppenhagen.de>
2018-08-25 16:46:39 +0200

    adjusts environment url as ios simulator can't handle 'localhost'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://127.0.0.1:8080'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://127.0.0.1:8080'
 };
